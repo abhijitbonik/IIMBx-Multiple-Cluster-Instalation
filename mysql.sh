3ac506fbe203b325bbb0b262c9d3ea6c638b53e7
abhisgithub <abhijitbonik@hotmail.com>
2017-09-25 06:16:47 +0530

    Remote Database connection error solved

--- mysql.sh
+++ mysql.sh
@@ -21,3 +21,13 @@ mysql -uroot -proot -e "CREATE DATABASE iimbx ;"
 echo "Importing the database on Mysql server"
 mysql -u root -proot iimbx < iimbx_21082017.sql
 
+
+echo "Commenting the bind bind-address to 127.0.0.1 for enabling remote log in"
+sudo -S <<< $1 sed -i 's/bind-address\s*=.*/#bind-address		= 127.0.0.1/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+#for now please change the ip address with web 1 and web 2 ipddress
+echo "Setting Permission for web 1"
+mysql -uroot -proot -e " GRANT ALL ON *.* TO root@'10.129.26.103' IDENTIFIED BY 'edx123';"
+
+echo "Setting permisiion for Web2"
+mysql -uroot -proot -e " GRANT ALL ON *.* TO root@'10.129.26.103' IDENTIFIED BY 'edx123';"
