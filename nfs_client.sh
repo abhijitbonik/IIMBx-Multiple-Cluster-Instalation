16d41b554b260be76cd7efdf5cfcba9158fbd61f
firuza <firya87@gmail.com>
2017-09-26 20:32:31 +0530

    install nfs common without confirmation

--- nfs_client.sh
+++ nfs_client.sh
@@ -1,7 +1,7 @@
 #install nfs client
 
 sudo -S <<< $1 apt-get update
-sudo -S <<< $1 apt-get install nfs-common
+sudo -S <<< $1 apt-get install nfs-common -y
 
 #and mount the directory
 sudo -S <<< $1 mount $2:/var/www/html/iimbx/sites/default/files /var/www/iimbx/sites/default/files
